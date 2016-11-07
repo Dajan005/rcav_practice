1a40c05ef943047c7953a3847ba6af7f5796c864
puts "Hello, World <Dajan005@users.noreply.github.com>
2016-11-06 18:01:18 -0600

    Completed assignment
    All three websites function as expected

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @num = params[:num].to_i
+    @sqrt = Math.sqrt(@num)
+  end
+
+  def random_number
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+
+  end
+
+  def payment
+    @rate = params[:rate].to_f/100
+    @term = params[:years].to_i
+    @principal = params[:principal].to_i
+    @i = (1+(@rate/100)/12)**(12/12)-1
+    @month = @term *12
+    @af = (1-(1/(1+@i))**@month)/@i
+
+    @payment = (@principal/@af).round
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+
+  get("/square_root/:num", { :controller => "calculations", :action => "square_root"})
+  get("/random/:min/:max", { :controller => "calculations", :action => "random_number"})
+  get("/payment/:rate/:years/:principal", { :controller => "calculations", :action => "payment"})
 end
